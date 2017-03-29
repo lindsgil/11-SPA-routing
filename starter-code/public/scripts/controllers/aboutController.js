eed5ec82d40816bbaca046521c7ed50f9a1f3547
Lindsay Gilbert <Lindsaygilbert722@gmail.com>
2017-03-28 17:01:24 -0700

    added script tags, page routes, page inits

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -3,8 +3,12 @@
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+    // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.init = function() {
+    console.log('Helloooo we are inside of aboutController');
+    $('main').hide();
+    $('#about').show();
+  };
 
   module.aboutController = aboutController;
 })(window);
